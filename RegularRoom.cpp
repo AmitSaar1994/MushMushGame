eb923b1823dfab556ba446cccfa51d20530c798c
AmitSaar1994 <39333634+AmitSaar1994@users.noreply.github.com>
2018-06-12 17:08:07 +0300

    Create RegularRoom.cpp

---
+++ RegularRoom.cpp
@@ -0,0 +1,17 @@
+#include "RegularRoom.h"
+#include "string"
+#include "string.h"
+
+
+using namespace std;
+
+
+RegularRoom::RegularRoom(int tunnel1, int tunnel2, int tunnel3) : Room(tunnel1, tunnel2, tunnel3)
+{}
+
+
+
+RegularRoom::~RegularRoom()
+{
+}
+
